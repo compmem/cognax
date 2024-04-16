966338efd87678046b8a9a7d5fb81811755ddeed
Ami Falk <amiafalk@gmail.com>
2024-04-16 17:20:46 -0400

    code ref

--- cognax/util.py
+++ cognax/util.py
@@ -13,5 +13,6 @@ def vmap_n(fn, n_times, *args, **kwargs):
     return fn(*args, **kwargs)
 
 
+# from https://gist.github.com/willwhitney/dd89cac6a5b771ccff18b06b33372c75?permalink_comment_id=4634557#gistcomment-4634557
 def tree_stack(trees):
     return jax.tree_util.tree_map(lambda *v: jnp.stack(v), *trees)
